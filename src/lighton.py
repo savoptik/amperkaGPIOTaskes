7bc5675cba6c3307c2239ba54da4a4da5071b7c4
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-04-07 21:29:59 +0300

    update lighton

--- src/lighton.py
+++ src/lighton.py
@@ -1,14 +1,18 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
+
 for i in [10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 24, 26]:
     GPIO.setup(i, GPIO.OUT)
+    time.sleep(0.5)
     GPIO.output(i, GPIO.HIGH)
 
-input()
+time.sleep(5)
 
 for i in [10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 24, 26]:
     GPIO.output(i, GPIO.LOW)
+    time.sleep(0.5)
 
 GPIO.cleanup()
 
