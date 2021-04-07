ec58358bfc32544b6926c9f14fd94383b8ad343b
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-04-08 01:11:16 +0300

    fixed task 5 switch

---
+++ src/switch.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(8, GPIO.IN)
+
+leds = [13, 19, 16]
+
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.output(10, GPIO.HIGH)
+
+try:
+    while True:
+        button = not GPIO.input(8)
+        for i in leds:
+            GPIO.output(i, button)
+        GPIO.output(24, not button)
+except KeyboardInterupt:
+    print('The program was stopped by keyboard.')
+finally:
+    GPIO.output(10, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.cleanup()
+    print('GPIO cleanup completed')
